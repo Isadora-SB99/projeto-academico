454085644f83bd273e162f0b2171857849e9bd8b
unknown <isadorabellaguarda@sou.faccat.br>
2025-05-14 19:49:58 -0300

    aula1

---
+++ academico/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+GENERO_CHOICES = [
+    ('M', 'Masculino'),
+    ('F', 'Feminino'),
+    ('NB', 'Não-binário'),
+    ('O', 'Outro'),
+]
+
+ESTADO_CIVIL_CHOICES = [
+    ('S', 'Solteiro'),
+    ('C', 'Casado'),
+    ('D', 'Divorciado'),
+    ('V', 'Viúvo'),
+]
+
+ESCOLARIDADE_CHOICES = [
+    ('fun', 'Fundamental'),
+    ('med', 'Médio'),
+    ('sup', 'Superior'),
+    ('pos', 'Pós-graduação'),
+    ('mes', 'Mestrado'),
+    ('dou', 'Doutorado'),
+]
+
+
+class Curso(models.Model):
+    nome = models.CharField("digite o nome: ", max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+
+class Aluno(models.Model):
+    nome = models.CharField("digite o nome: ", max_length=100)
+    data_nascimento = models.DateField("informe a data de nascimento: ", )
+    cpf = models.CharField("digite o cpf: ", max_length=14, unique=True)
+    genero = models.CharField(
+        "selecione o gênero: ", max_length=2, choices=GENERO_CHOICES)
+    estado_civil = models.CharField("selecione o estado civil: ",
+                                    max_length=30, choices=ESTADO_CIVIL_CHOICES)
+    escolaridade = models.CharField("selecione o nível de escolaridade: ",
+                                    max_length=50, choices=ESCOLARIDADE_CHOICES)
+    curso = models.ForeignKey(
+        Curso, on_delete=models.CASCADE, verbose_name="selecione o curso: ",)
+
+    def __str__(self):
+        return self.nome
